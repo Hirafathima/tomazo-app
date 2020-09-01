2bd7c5fea1d977ad2092972edc9286e4332ca0b6
Hirafathima <hirafathima125@gmail.com>
2020-09-01 18:08:07 +0530

    msg

--- src/components/pages/info.js
+++ src/components/pages/info.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const API_KEY = '';
 export default axios.create({
   method:'get',
   baseURL: 'https://developers.zomato.com/api/v2.1',
   headers: { 
     'Accept': 'application/json', 
-    'user-key': '3c72e73dbc57072e925dd603c1480fb7'
+    'user-key': API_KEY
   }
 });
